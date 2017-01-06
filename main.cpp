477202947786334085e7ec3a2912dd146079a80a
juanmafc <juanmafc25@gmail.com>
2017-01-06 16:10:37 -0300

    Main modified to run a test
    Also, commented coded that was used to test if the pixel access
    algorithim was working

--- main.cpp
+++ main.cpp
@@ -7,17 +7,20 @@
 #include <Magick++.h>
 #include <iostream>
 
+#include "DisplayTests.h"
+
 using namespace std;
 using namespace Magick;
 
 
-int main(int argc,char **argv)
-{
-  InitializeMagick(*argv);
+int main(int argc,char **argv){
+  InitializeMagick("bin/Debug");
 
   // Construct the image object. Seperating image construction from the
   // the read operation ensures that a failure to read the image file
   // doesn't render the image object useless.
+
+/*
   Image image;
   try {
     // Read a file into image object
@@ -27,13 +30,75 @@ int main(int argc,char **argv)
     image.crop( Geometry(100,100, 100, 100) );
 
     // Write the image to a file
-    image.write( "logo.png" );
+    image.write( "logo3.jpg" );
   }
   catch( Exception &error_ )
     {
       cout << "Caught exception: " << error_.what() << endl;
       return 1;
     }
+*/
+
+  DisplayTests::displayASingleImage();
+
+/////////////////////////////////////////////////
+//  Picture picture("janna.png");
+//  Geometry geom(193,84);
+//  ColorRGB color;
+//  color.red(1);
+//  color.blue(1);
+//
+//  Image destino(geom, color);
+//  destino.modifyImage();
+//
+//
+//  Pixels view(destino);
+//  // Set all pixels in region anchored at 38x36, with size 160x230 to green.
+//  size_t columns = 193; size_t rows = 84;
+//  PixelPacket *pixels = view.get(0,0,columns,rows);
+//  /*
+//  for ( ssize_t row = 0; row < rows ; ++row )
+//        for ( ssize_t column = 0; column < columns ; ++column )
+//            *pixels++=green;
+//    */
+//
+//
+//    Image janna("janna.png");
+//
+//
+//
+//    PixelPacket *jannaPix = janna.getPixels(0,0,193,84);
+//    int pixelNro = 0;
+//  for (int row = 0; row < 84; row++){
+//        for (int column = 0; column < 193; column++){
+//            /*
+//            float red = picture.getRedAt(row, column);
+//            float green = picture.getGreenAt(row, column);
+//            float blue = picture.getBlueAt(row, column);
+//            */
+//
+//
+//            Color color = jannaPix[193 * row + column];
+////            Color color = jannaPix[pixelNro];
+//            pixelNro++;
+//            //ColorRGB rgb(red, green, blue);
+//            //ColorRGB rgb(color);
+//            //*pixels++=rgb;
+//            *pixels++=color;
+//
+//            /*
+//            Magick::PixelPacket *pixeles= this->image.getPixels(0,0,this->image.columns(),this->image.rows());
+//            Magick::Color color = pixeles[this->image.rows() * row + column];
+//            Magick::ColorRGB rgb(color);
+//*/
+//
+//        }
+//    }
+//
+//
+//    view.sync();
+//    destino.write("canvas.bmp");
+////////////////////////////////////
   return 0;
 }
 
@@ -84,7 +149,7 @@ int main(int argc,char **argv)
 
 
 
-
+/*
 static ALGIF_ANIMATION *load(char const *name) {
     ALLEGRO_PATH *examples = al_create_path_for_directory("examples");
     ALLEGRO_PATH *path = al_create_path(name);
@@ -102,7 +167,7 @@ static ALGIF_ANIMATION *load(char const *name) {
 
     return gif;
 }
-
+*/
 
 
 /*
