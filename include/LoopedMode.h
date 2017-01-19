c8d5eaabd3c1ea6d1793f18e8a788886b14df056
juanmafc <juanmafc25@gmail.com>
2017-01-19 02:32:56 -0300

    LoopedMode added
    It extends from RenderingMode

---
+++ include/LoopedMode.h
@@ -0,0 +1,22 @@
+#ifndef LOOPEDMODE_H
+#define LOOPEDMODE_H
+
+#include <RenderingMode.h>
+
+
+class LoopedMode : public RenderingMode
+{
+    public:
+        LoopedMode(Screen* screen, Gif* gif);
+        virtual ~LoopedMode();
+
+        void render();
+
+        virtual void handleEvent(ALLEGRO_EVENT* event);
+
+    protected:
+
+    private:
+};
+
+#endif // LOOPEDMODE_H
---
+++ src/LoopedMode.cpp
@@ -0,0 +1,23 @@
+#include "LoopedMode.h"
+
+LoopedMode::LoopedMode(Screen* screen, Gif* gif): RenderingMode(screen, gif) {
+    //ctor
+}
+
+LoopedMode::~LoopedMode()
+{
+    //dtor
+}
+
+void LoopedMode::render() {
+    this->screen->displayPicture( this->gif->getFrameForAGivenTime( al_get_time() ) );
+}
+
+void LoopedMode::handleEvent(ALLEGRO_EVENT* event) {
+    RenderingMode::handleEvent(event);
+
+    if (event->type == ALLEGRO_EVENT_TIMER) {
+        this->redraw = true;
+    }
+}
+
